9a8c48fa8cb71f53fa290afe26066cf3fd4d1f6f
Alex Wong <akwong14@myseneca.ca>
2024-05-09 23:54:36 -0400

    fixed broken links and updated js/css

--- js/javascript.js
+++ js/javascript.js
@@ -1,43 +1,12 @@
-/* 
-*      Name:       <ALEX WONG>
-*      Student ID: <031842149>
-*      Date:       <April 14 2022>
-*      Section:    <WEB222 - NCC>
-*/
+const linkButton = document.querySelectorAll(".icons");
 
-// this is for form submission validation
+linkButton.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
 
-window.onload = function() {
-  let form = document.querySelector('#contactMe');
+    const getLink = e.target.parentElement.getAttribute("href");
+    const copyLinkToClipBoard = navigator.clipboard.writeText(getLink);
 
-  form.onsubmit = function(event) {
-    // validate input
-    if(!form.checkValidity()) {
-      // if not valid
-      event.preventDefault();
-      return false;
-    }
-    return true;
-  }
-}
-
-function show(){
-  if (document.getElementById("contactMe-hourlyRate").classList.contains("hide")) {
-    document.getElementById("contactMe-hourlyRate").classList.remove("hide");
-  }
-
-  if(document.getElementById("hourlyRate").required == false) {
-    document.getElementById("hourlyRate").required = true;
-  }
-}
-
-function hide(){
-  if (!document.getElementById("contactMe-hourlyRate").classList.contains("hide")) {
-    document.getElementById("contactMe-hourlyRate").classList.add("hide");
-  }
-
-  if (document.getElementById("hourlyRate").required == true) {
-    document.getElementById("hourlyRate").required = false;
-  }
-  document.getElementById("hourlyRate").value = "";
-}
+    alert("Link has been copied to clipboard");
+  });
+});
